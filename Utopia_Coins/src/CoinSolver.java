678f0925a01a6c5cb1e57d7c6cb0519bb9de0148
Artis <mundurs.artis@gmail.com>
2021-05-08 14:03:50 +0200

    Maybe

---
+++ Utopia_Coins/src/CoinSolver.java
@@ -0,0 +1,26 @@
+public class CoinSolver {
+
+    final static int coins[] = {1, 7, 10, 22};
+
+    static int minCoins(int value)
+    {
+        int table[] = new int[value + 1];
+        table[0] = 0;
+        for (int i = 1; i <= value; i++)
+            table[i] = Integer.MAX_VALUE;
+        for (int i = 1; i <= value; i++)
+        {
+            for (int j = 0; j < 4; j++)
+                if (coins[j] <= i)
+                {
+                    int sub_res = table[i - coins[j]];
+                    if (sub_res != Integer.MAX_VALUE
+                            && sub_res + 1 < table[i])
+                        table[i] = sub_res + 1;
+                }
+        }
+        if(table[value]==Integer.MAX_VALUE)
+            return -1;
+        return table[value];
+    }
+}
---
+++ nQueens/src/nQueenSolver.java
@@ -0,0 +1,44 @@
+public class nQueenSolver {
+
+    final int Queens = 4;
+
+    boolean solveQueen() {
+        int board[][] = {{0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}};
+        if (qSolverUtil(board, 0) == false) {
+            return false;
+        }
+        return true;
+    }
+
+    boolean qSolverUtil(int board[][], int col) {
+        if (col >= Queens)
+            return true;
+
+        for (int i = 0; i < Queens; i++) {
+            if (isSafe(board, i, col)) {
+                board[i][col] = 1;
+                if (qSolverUtil(board, col + 1) == true)
+                    return true;
+                board[i][col] = 0;
+            }
+        }
+        return false;
+    }
+
+    boolean isSafe(int board[][], int row, int col) {
+        int i, j;
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+        for (i = row, j = col; j >= 0 && i < Queens; i++, j--)
+            if (board[i][j] == 1)
+                return false;
+        for (i = 0; i < col; i++)
+            if (board[row][i] == 1)
+                return false;
+        return true;
+    }
+}
