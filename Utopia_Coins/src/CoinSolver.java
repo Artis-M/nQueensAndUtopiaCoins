8c6c3b15f46a4ec097cbae4afc6d47e1f9090b51
Artis <mundurs.artis@gmail.com>
2021-05-08 14:19:55 +0200

    Update CoinSolver.java

--- Utopia_Coins/src/CoinSolver.java
+++ Utopia_Coins/src/CoinSolver.java
@@ -7,16 +7,20 @@ public class CoinSolver {
         int table[] = new int[value + 1];
         table[0] = 0;
         for (int i = 1; i <= value; i++)
+        {
             table[i] = Integer.MAX_VALUE;
+        }
         for (int i = 1; i <= value; i++)
         {
             for (int j = 0; j < 4; j++)
                 if (coins[j] <= i)
                 {
                     int sub_res = table[i - coins[j]];
-                    if (sub_res != Integer.MAX_VALUE
-                            && sub_res + 1 < table[i])
+                    if (sub_res != Integer.MAX_VALUE && sub_res + 1 < table[i])
+                    {
                         table[i] = sub_res + 1;
+                    }
+
                 }
         }
         if(table[value]==Integer.MAX_VALUE)
